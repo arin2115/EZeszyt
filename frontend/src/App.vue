38bef2d86628d9cc752d0d2a252b50781731e608
Marcin Kowalicki <kowalickimarcin13@gmail.com>
2021-09-06 17:19:37 +0200

    move drawer and app bar to another component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,68 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar
-      color="blue"
-      dark
-      absolute
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>E-Zeszyt</v-toolbar-title>
-    </v-app-bar>
-<v-navigation-drawer v-model="drawer" temporary absolute>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            E-Zeszyt
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Twój wirtualny zeszyt
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-</v-navigation-drawer>
+   <Menu></Menu>
   </v-app>
 </template>
 
 <script>
+import Menu from './components/Menu'
+
 export default {
   name: 'App',
 
   components: {
-
+    Menu
   },
 
-  data: () => ({
-        drawer: null,
-        items: [
-          { title: 'Strona główna', icon: 'mdi-view-dashboard' },
-          { title: 'Zeszyty', icon: 'mdi-notebook' },
-          { title: 'Podręczniki', icon: 'mdi-book' },
-          { title: 'Czat', icon: 'mdi-forum' },
-          { title: 'Ustawienia', icon: 'mdi-cog' },
-          { title: 'Co to jest?', icon: 'mdi-help-box' },
-        ],
-        right: null,
-  }),
+  data: () => ({}),
 };
 </script>
--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -1,12 +1,21 @@
 <template>
-<v-navigation-drawer v-bind="drawer" v-model="drawer" temporary absolute>
+<div>
+    <v-app-bar
+      color="blue"
+      dark
+      absolute
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>E-Zeszyt</v-toolbar-title>
+    </v-app-bar>
+<v-navigation-drawer v-model="drawer" temporary absolute>
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="text-h6">
-            Application
+            E-Zeszyt
           </v-list-item-title>
           <v-list-item-subtitle>
-            subtext
+            Twój wirtualny zeszyt
           </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
@@ -32,21 +41,24 @@
         </v-list-item>
       </v-list>
 </v-navigation-drawer>
+</div>
 </template>
 
+
 <script>
-  export default {
-    props: ['initialCounter'],
-    data () {
-      return {
+export default {
+  name: 'Menu',
+  data: () => ({
         drawer: null,
         items: [
-          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-          { title: 'Photos', icon: 'mdi-image' },
-          { title: 'About', icon: 'mdi-help-box' },
+          { title: 'Strona główna', icon: 'mdi-view-dashboard' },
+          { title: 'Zeszyty', icon: 'mdi-notebook' },
+          { title: 'Podręczniki', icon: 'mdi-book' },
+          { title: 'Czat', icon: 'mdi-forum' },
+          { title: 'Ustawienia', icon: 'mdi-cog' },
+          { title: 'Co to jest?', icon: 'mdi-help-box' },
         ],
         right: null,
-      }
-    },
-  }
+  }),
+};
 </script>
